0373c68beaf112dc678ddbdd7a3789864739f54c
gamerlegume <mumu.unicorn@gmail.com>
2026-04-22 15:03:35 -0500

    big pp ahahahahahahaa :3

---
+++ gpstore.php
@@ -0,0 +1,106 @@
+<html><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <link rel="preconnect" href="https://fonts.googleapis.com/">
+        <link rel="preconnect" href="https://fonts.gstatic.com/" crossorigin="">
+        <link href="./gpstore.php_files/css2" rel="stylesheet">
+
+        <style>
+            h1 {text-align: center; font-family:'Nunito', sans-serif; color:hotpink; padding: 15px;}
+            p {text-align: center; font-family:'Nunito', sans-serif; color:lightpink;}
+            div {text-align: center;}
+            img 
+            {
+                display: block;
+                margin-left: auto;
+                margin-right: auto;
+                width: 75%; /* Optional: set a width smaller than the container */
+            }
+            td 
+            {
+                border: 1px solid lightgray;
+                border-radius: 10px;
+            }
+            img:hover 
+            {
+                transform: scale(1.05);
+                transition: 0.3s;
+            }
+            
+            table 
+            {
+                margin: auto;
+                border-radius: 10px;
+                border-spacing: 20px;
+            }
+
+            .button 
+            {
+                border: none;
+                color: white;
+                padding: 16px 32px;
+                text-align: center;
+                text-decoration: none;
+                display: inline-block;
+                font-size: 16px;
+                margin: 4px 2px;
+                transition-duration: 0.3s;
+                cursor: pointer;
+            }
+
+            .button1 
+            {
+                background-color: white; 
+                color: black; 
+                border: 2px solid hotpink;
+                border-radius: 10px;
+            }
+
+            .button1:hover 
+            {
+                background-color: pink;
+                color: white;
+                border-radius: 10px;
+            }
+        </style>
+    </head>
+
+
+
+    <body style="background-color:Lavender">
+        
+        <h1>Stuffie Store<hr></h1>
+
+        <table width="75%" border="0" style="background-color:white">    
+            <tbody><tr>    
+            <td><br><img src="./gpstore.php_files/IMG_8163.jpg" alt="erawr" width="300" height="auto"><p><button class="button button1">ADD TO CART</button></p><p>erawr</p><p>$500</p><p>DESCRIPTION OHH YEAHHH</p><br></td>
+            <td><br><img src="./gpstore.php_files/IMG_8167.jpg" alt="urawr" width="300" height="auto"><p>urawr</p><p>$2000</p><p>DESCRIPTION OHH YEAHHH</p><br></td>
+            <td><br><img src="./gpstore.php_files/IMG_8165.jpg" alt="mirawr" width="300" height="auto"><p>mirawr</p><p>$3</p><p>DESCRIPTION OHH YEAHHH</p><br></td>
+            </tr>
+
+            <tr>    
+            <td><br><img src="./gpstore.php_files/IMG_8166.jpg" alt="tirawr" width="300" height="auto"><p>tirawr</p><p>$500</p><p>DESCRIPTION OHH YEAHHH</p><br></td>
+            <td><br><img src="./gpstore.php_files/IMG_8168.jpg" alt="quagsire" width="300" height="auto"><p>quagsire</p><p>$500</p><p>DESCRIPTION OHH YEAHHH</p><br></td>
+            <td><br><img src="./gpstore.php_files/IMG_8169.jpg" alt="clodsire" width="300" height="auto"><p>clodsire</p><p>$500</p><p>DESCRIPTION OHH YEAHHH</p><br></td>
+            </tr>
+
+            <tr>    
+            <td><br><img src="./gpstore.php_files/IMG_8172.jpg" alt="tamago" width="300" height="auto"><p>tamago</p><p>$500</p><p>DESCRIPTION OHH YEAHHH</p><br></td>
+            <td><br><img src="./gpstore.php_files/IMG_8171.jpg" alt="shibata" width="300" height="auto"><p>shibata</p><p>$500</p><p>DESCRIPTION OHH YEAHHH</p><br></td>
+            <td><br><img src="./gpstore.php_files/IMG_8170.jpg" alt="shibata BIG" width="300" height="auto"><p>shibata BIG</p><p>$500</p><p>DESCRIPTION OHH YEAHHH</p><br></td>
+            </tr>
+
+            <tr>    
+            <td><br><img src="./gpstore.php_files/IMG_8177.jpg" alt="burnt chibatta" width="300" height="auto"><p>burnt chibatta</p><p>$500</p><p>DESCRIPTION OHH YEAHHH</p><br></td>
+            <td><br><img src="./gpstore.php_files/IMG_8178.jpg" alt="shibata roll" width="300" height="auto"><p>shibata roll</p><p>$500</p><p>DESCRIPTION OHH YEAHHH</p><br></td>
+            <td><br><img src="./gpstore.php_files/IMG_8176.jpg" alt="sharkie" width="300" height="auto"><p>sharkie</p><p>$500</p><p>DESCRIPTION OHH YEAHHH</p><br></td>
+            </tr>
+
+            <tr>    
+            <td><br><img src="./gpstore.php_files/IMG_8164.jpg" alt="hedhog" width="300" height="auto"><p>hedhog</p><p>$500</p><p>DESCRIPTION OHH YEAHHH</p><br></td>
+            <td><br><img src="./gpstore.php_files/IMG_8175.jpg" alt="coronavirus" width="300" height="auto"><p>coronavirus</p><p>$500</p><p>DESCRIPTION OHH YEAHHH</p><br></td>
+            <td><br><img src="./gpstore.php_files/IMG_8173.jpg" alt="skunkie" width="300" height="auto"><p>skunkie</p><p>$500</p><p>DESCRIPTION OHH YEAHHH</p><br></td>
+            </tr>
+        </tbody></table>
+
+
+            
+</body></html>
