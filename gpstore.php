68a17dc5783035465475c4fa9b25be50280f4943
gamerlegume <mumu.unicorn@gmail.com>
2026-04-23 17:19:07 -0500

    Update image sources to use external links

--- gpstore.php
+++ gpstore.php
@@ -1,7 +1,8 @@
-<html><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <link rel="preconnect" href="https://fonts.googleapis.com/">
-        <link rel="preconnect" href="https://fonts.gstatic.com/" crossorigin="">
-        <link href="./gpstore.php_files/css2" rel="stylesheet">
+<html>
+    <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,200..1000;1,200..1000&display=swap" rel="stylesheet">
 
         <style>
             h1 {text-align: center; font-family:'Nunito', sans-serif; color:hotpink; padding: 15px;}
@@ -70,37 +71,40 @@
         <h1>Stuffie Store<hr></h1>
 
         <table width="75%" border="0" style="background-color:white">    
-            <tbody><tr>    
-            <td><br><img src="./gpstore.php_files/IMG_8163.jpg" alt="erawr" width="300" height="auto"><p><button class="button button1">ADD TO CART</button></p><p>erawr</p><p>$500</p><p>DESCRIPTION OHH YEAHHH</p><br></td>
-            <td><br><img src="./gpstore.php_files/IMG_8167.jpg" alt="urawr" width="300" height="auto"><p>urawr</p><p>$2000</p><p>DESCRIPTION OHH YEAHHH</p><br></td>
-            <td><br><img src="./gpstore.php_files/IMG_8165.jpg" alt="mirawr" width="300" height="auto"><p>mirawr</p><p>$3</p><p>DESCRIPTION OHH YEAHHH</p><br></td>
+            <tr>    
+            <td><br><img src="https://media.discordapp.net/attachments/1488268700904849598/1495956488852865045/IMG_8163.jpg?ex=69e8218c&is=69e6d00c&hm=1dec51d065ba9068dc265390d75afc9870d3b1358b3e5c7715fc22f976f064fb&=&format=webp&width=1152&height=864" alt="erawr" width="300" height="auto"/><p><button class="button button1">ADD TO CART</button></p><p>erawr</p><p>$500</p><p>DESCRIPTION OHH YEAHHH</p><br></td>
+            <td><br><img src="https://media.discordapp.net/attachments/1488268700904849598/1495956490794565793/IMG_8167.jpg?ex=69e8218c&is=69e6d00c&hm=13563c535c1a3f587fc232e611c2ed78a2bd95c98be7818b0e48eecf4ab817b2&=&format=webp&width=1152&height=864" alt="urawr" width="300" height="auto" /><p>urawr</p><p>$2000</p><p>DESCRIPTION OHH YEAHHH</p><br></td>
+            <td><br><img src="https://media.discordapp.net/attachments/1488268700904849598/1495956489754378260/IMG_8165.jpg?ex=69e8218c&is=69e6d00c&hm=ffda1cc5fa1d35358abb5433999c59666bdb2b547b31ad46e3772a497e580850&=&format=webp&width=1152&height=864" alt="mirawr" width="300" height="auto" /><p>mirawr</p><p>$3</p><p>DESCRIPTION OHH YEAHHH</p><br></td>
             </tr>
 
             <tr>    
-            <td><br><img src="./gpstore.php_files/IMG_8166.jpg" alt="tirawr" width="300" height="auto"><p>tirawr</p><p>$500</p><p>DESCRIPTION OHH YEAHHH</p><br></td>
-            <td><br><img src="./gpstore.php_files/IMG_8168.jpg" alt="quagsire" width="300" height="auto"><p>quagsire</p><p>$500</p><p>DESCRIPTION OHH YEAHHH</p><br></td>
-            <td><br><img src="./gpstore.php_files/IMG_8169.jpg" alt="clodsire" width="300" height="auto"><p>clodsire</p><p>$500</p><p>DESCRIPTION OHH YEAHHH</p><br></td>
+            <td><br><img src="https://media.discordapp.net/attachments/1488268700904849598/1495956490312356011/IMG_8166.jpg?ex=69e8218c&is=69e6d00c&hm=4544b21152ef2e097cedf8cc8181232337097d8ce305dfc1022526f161930667&=&format=webp&width=1152&height=864" alt="tirawr" width="300" height="auto"/><p>tirawr</p><p>$500</p><p>DESCRIPTION OHH YEAHHH</p><br></td>
+            <td><br><img src="https://media.discordapp.net/attachments/1488268700904849598/1495956491214131300/IMG_8168.jpg?ex=69e8218d&is=69e6d00d&hm=c9414b9402e0f01544170745a6e886ba0fbd5a59313d4f1491328f40d0fd3f2c&=&format=webp&width=1152&height=864" alt="quagsire" width="300" height="auto" /><p>quagsire</p><p>$500</p><p>DESCRIPTION OHH YEAHHH</p><br></td>
+            <td><br><img src="https://media.discordapp.net/attachments/1488268700904849598/1495956491901861978/IMG_8169.jpg?ex=69e8218d&is=69e6d00d&hm=4d1b68c0435689ce8707eae8840bd64e811efec7b7404ba3e6e9ada96a801a5d&=&format=webp&width=1152&height=864" alt="clodsire" width="300" height="auto" /><p>clodsire</p><p>$500</p><p>DESCRIPTION OHH YEAHHH</p><br></td>
             </tr>
 
             <tr>    
-            <td><br><img src="./gpstore.php_files/IMG_8172.jpg" alt="tamago" width="300" height="auto"><p>tamago</p><p>$500</p><p>DESCRIPTION OHH YEAHHH</p><br></td>
-            <td><br><img src="./gpstore.php_files/IMG_8171.jpg" alt="shibata" width="300" height="auto"><p>shibata</p><p>$500</p><p>DESCRIPTION OHH YEAHHH</p><br></td>
-            <td><br><img src="./gpstore.php_files/IMG_8170.jpg" alt="shibata BIG" width="300" height="auto"><p>shibata BIG</p><p>$500</p><p>DESCRIPTION OHH YEAHHH</p><br></td>
+            <td><br><img src="https://media.discordapp.net/attachments/1488268700904849598/1495956493428592760/IMG_8172.jpg?ex=69e8218d&is=69e6d00d&hm=45cbaddffc103e48d4ecf7ac65512724c6643367c78cc81814229b3ab6ab00e9&=&format=webp&width=1152&height=864" alt="tamago" width="300" height="auto"/><p>tamago</p><p>$500</p><p>DESCRIPTION OHH YEAHHH</p><br></td>
+            <td><br><img src="https://media.discordapp.net/attachments/1488268700904849598/1495956492908494868/IMG_8171.jpg?ex=69e8218d&is=69e6d00d&hm=1368586784d2a8a10fa010d80e050440d6b13d6f7713ca5b2e536a52d3a292b1&=&format=webp&width=1152&height=864" alt="shibata" width="300" height="auto" /><p>shibata</p><p>$500</p><p>DESCRIPTION OHH YEAHHH</p><br></td>
+            <td><br><img src="https://media.discordapp.net/attachments/1488268700904849598/1495956492434800670/IMG_8170.jpg?ex=69e8218d&is=69e6d00d&hm=094adb1acdee0f7e188e2846ce31ecd4e70590549d740462a52369a2caac216d&=&format=webp&width=1152&height=864" alt="shibata BIG" width="300" height="auto" /><p>shibata BIG</p><p>$500</p><p>DESCRIPTION OHH YEAHHH</p><br></td>
             </tr>
 
             <tr>    
-            <td><br><img src="./gpstore.php_files/IMG_8177.jpg" alt="burnt chibatta" width="300" height="auto"><p>burnt chibatta</p><p>$500</p><p>DESCRIPTION OHH YEAHHH</p><br></td>
-            <td><br><img src="./gpstore.php_files/IMG_8178.jpg" alt="shibata roll" width="300" height="auto"><p>shibata roll</p><p>$500</p><p>DESCRIPTION OHH YEAHHH</p><br></td>
-            <td><br><img src="./gpstore.php_files/IMG_8176.jpg" alt="sharkie" width="300" height="auto"><p>sharkie</p><p>$500</p><p>DESCRIPTION OHH YEAHHH</p><br></td>
+            <td><br><img src="https://media.discordapp.net/attachments/1488268700904849598/1495956523464134707/IMG_8177.jpg?ex=69e82194&is=69e6d014&hm=ee9eddcb4b96a8f371328f929190e3f20bd0f43625ad96b3dbec4ac6718e6b92&=&format=webp&width=1152&height=864" alt="burnt chibatta" width="300" height="auto"/><p>burnt chibatta</p><p>$500</p><p>DESCRIPTION OHH YEAHHH</p><br></td>
+            <td><br><img src="https://media.discordapp.net/attachments/1488268700904849598/1495956524168773752/IMG_8178.jpg?ex=69e82194&is=69e6d014&hm=f930def8bfb399b6bf32308911be8975afea97282a660b42f38c675e285b2d66&=&format=webp&width=1152&height=864" alt="shibata roll" width="300" height="auto" /><p>shibata roll</p><p>$500</p><p>DESCRIPTION OHH YEAHHH</p><br></td>
+            <td><br><img src="https://media.discordapp.net/attachments/1488268700904849598/1495956522713481246/IMG_8176.jpg?ex=69e82194&is=69e6d014&hm=4428f61cd96890883733902fe43f48345cdf158c9b3b7ebb0be371b8741d60c0&=&format=webp&width=1152&height=864" alt="sharkie" width="300" height="auto" /><p>sharkie</p><p>$500</p><p>DESCRIPTION OHH YEAHHH</p><br></td>
             </tr>
 
             <tr>    
-            <td><br><img src="./gpstore.php_files/IMG_8164.jpg" alt="hedhog" width="300" height="auto"><p>hedhog</p><p>$500</p><p>DESCRIPTION OHH YEAHHH</p><br></td>
-            <td><br><img src="./gpstore.php_files/IMG_8175.jpg" alt="coronavirus" width="300" height="auto"><p>coronavirus</p><p>$500</p><p>DESCRIPTION OHH YEAHHH</p><br></td>
-            <td><br><img src="./gpstore.php_files/IMG_8173.jpg" alt="skunkie" width="300" height="auto"><p>skunkie</p><p>$500</p><p>DESCRIPTION OHH YEAHHH</p><br></td>
+            <td><br><img src="https://media.discordapp.net/attachments/1488268700904849598/1495956489330757773/IMG_8164.jpg?ex=69e8218c&is=69e6d00c&hm=641fd70476ec228e42e86665c13e2dfae3a0ad2d382efe98c5378a73700e932e&=&format=webp&width=1152&height=864" alt="hedhog" width="300" height="auto"/><p>hedhog</p><p>$500</p><p>DESCRIPTION OHH YEAHHH</p><br></td>
+            <td><br><img src="https://media.discordapp.net/attachments/1488268700904849598/1495956521723367424/IMG_8175.jpg?ex=69e82194&is=69e6d014&hm=33a8da0292314e44abe2f51b02bb3d9fb2b9207510ed2b5de4428dcbaca9d0cd&=&format=webp&width=1152&height=864" alt="coronavirus" width="300" height="auto" /><p>coronavirus</p><p>$500</p><p>DESCRIPTION OHH YEAHHH</p><br></td>
+            <td><br><img src="https://media.discordapp.net/attachments/1488268700904849598/1495956520683307088/IMG_8173.jpg?ex=69e82194&is=69e6d014&hm=2c93ed54d5a9be5d03f17028880dc511d84653c6dea6525bd2ff2a326a2f0c90&=&format=webp&width=1152&height=864" alt="skunkie" width="300" height="auto" /><p>skunkie</p><p>$500</p><p>DESCRIPTION OHH YEAHHH</p><br></td>
             </tr>
-        </tbody></table>
+        </table>
 
 
-            
-</body></html>
+        <?php
+
+        ?>
+    </body>
+</html>
