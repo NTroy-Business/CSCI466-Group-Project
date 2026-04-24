9478f678d2ffccba7caa0a54e2c25f997b57320d
gamerlegume <mumu.unicorn@gmail.com>
2026-04-23 21:16:07 -0500

    Replace image URLs with new hosted images

--- gpstore.php
+++ gpstore.php
@@ -157,7 +157,7 @@ if(isset($_POST['addtocart']))
             <tr>
                 <td>
                     <br>
-                    <img src="https://media.discordapp.net/attachments/1488268700904849598/1495956488852865045/IMG_8163.jpg?ex=69e8218c&is=69e6d00c&hm=1dec51d065ba9068dc265390d75afc9870d3b1358b3e5c7715fc22f976f064fb&=&format=webp&width=1152&height=864" alt="erawr" width="300" height="auto"/>
+                    <img src="https://students.cs.niu.edu/~z1977897/erawr.jpg" alt="erawr" width="300" height="auto"/>
                     <form method="post">
                         <button class="button button1" type="submit" name="addtocart">ADD TO CART</button>
                         <input type="hidden" name="stuffie_id" value="S001">
@@ -171,7 +171,7 @@ if(isset($_POST['addtocart']))
 
                 <td>
                     <br>
-                    <img src="https://media.discordapp.net/attachments/1488268700904849598/1495956490794565793/IMG_8167.jpg?ex=69e8218c&is=69e6d00c&hm=13563c535c1a3f587fc232e611c2ed78a2bd95c98be7818b0e48eecf4ab817b2&=&format=webp&width=1152&height=864" alt="urawr" width="300" height="auto"/>
+                    <img src="https://students.cs.niu.edu/~z1977897/urawr.jpg" alt="urawr" width="300" height="auto"/>
                     <form method="post">
                         <button class="button button1" type="submit" name="addtocart">ADD TO CART</button>
                         <input type="hidden" name="stuffie_id" value="S002">
@@ -185,7 +185,7 @@ if(isset($_POST['addtocart']))
 
                 <td>
                     <br>
-                    <img src="https://media.discordapp.net/attachments/1488268700904849598/1495956489754378260/IMG_8165.jpg?ex=69e8218c&is=69e6d00c&hm=ffda1cc5fa1d35358abb5433999c59666bdb2b547b31ad46e3772a497e580850&=&format=webp&width=1152&height=864" alt="mirawr" width="300" height="auto"/>
+                    <img src="https://students.cs.niu.edu/~z1977897/mirawr.jpg" alt="mirawr" width="300" height="auto"/>
                     <form method="post">
                         <button class="button button1" type="submit" name="addtocart">ADD TO CART</button>
                         <input type="hidden" name="stuffie_id" value="S003">
@@ -201,7 +201,7 @@ if(isset($_POST['addtocart']))
             <tr>
                 <td>
                     <br>
-                    <img src="https://media.discordapp.net/attachments/1488268700904849598/1495956490312356011/IMG_8166.jpg?ex=69e8218c&is=69e6d00c&hm=4544b21152ef2e097cedf8cc8181232337097d8ce305dfc1022526f161930667&=&format=webp&width=1152&height=864" alt="tirawr" width="300" height="auto"/>
+                    <img src="https://students.cs.niu.edu/~z1977897/tirawr.jpg" alt="tirawr" width="300" height="auto"/>
                     <form method="post">
                         <button class="button button1" type="submit" name="addtocart">ADD TO CART</button>
                         <input type="hidden" name="stuffie_id" value="S004">
@@ -215,7 +215,7 @@ if(isset($_POST['addtocart']))
 
                 <td>
                     <br>
-                    <img src="https://media.discordapp.net/attachments/1488268700904849598/1495956491214131300/IMG_8168.jpg?ex=69e8218d&is=69e6d00d&hm=c9414b9402e0f01544170745a6e886ba0fbd5a59313d4f1491328f40d0fd3f2c&=&format=webp&width=1152&height=864" alt="quagsire" width="300" height="auto"/>
+                    <img src="https://students.cs.niu.edu/~z1977897/quagsire.jpg" alt="quagsire" width="300" height="auto"/>
                     <form method="post">
                         <button class="button button1" type="submit" name="addtocart">ADD TO CART</button>
                         <input type="hidden" name="stuffie_id" value="S005">
@@ -229,7 +229,7 @@ if(isset($_POST['addtocart']))
 
                 <td>
                     <br>
-                    <img src="https://media.discordapp.net/attachments/1488268700904849598/1495956491901861978/IMG_8169.jpg?ex=69e8218d&is=69e6d00d&hm=4d1b68c0435689ce8707eae8840bd64e811efec7b7404ba3e6e9ada96a801a5d&=&format=webp&width=1152&height=864" alt="clodsire" width="300" height="auto"/>
+                    <img src="https://students.cs.niu.edu/~z1977897/clodsire.jpg" alt="clodsire" width="300" height="auto"/>
                     <form method="post">
                         <button class="button button1" type="submit" name="addtocart">ADD TO CART</button>
                         <input type="hidden" name="stuffie_id" value="S006">
@@ -245,7 +245,7 @@ if(isset($_POST['addtocart']))
             <tr>
                 <td>
                     <br>
-                    <img src="https://media.discordapp.net/attachments/1488268700904849598/1495956493428592760/IMG_8172.jpg?ex=69e8218d&is=69e6d00d&hm=45cbaddffc103e48d4ecf7ac65512724c6643367c78cc81814229b3ab6ab00e9&=&format=webp&width=1152&height=864" alt="tamago" width="300" height="auto"/>
+                    <img src="https://students.cs.niu.edu/~z1977897/tamago.jpg" alt="tamago" width="300" height="auto"/>
                     <form method="post">
                         <button class="button button1" type="submit" name="addtocart">ADD TO CART</button>
                         <input type="hidden" name="stuffie_id" value="S007">
@@ -259,7 +259,7 @@ if(isset($_POST['addtocart']))
 
                 <td>
                     <br>
-                    <img src="https://media.discordapp.net/attachments/1488268700904849598/1495956492908494868/IMG_8171.jpg?ex=69e8218d&is=69e6d00d&hm=1368586784d2a8a10fa010d80e050440d6b13d6f7713ca5b2e536a52d3a292b1&=&format=webp&width=1152&height=864" alt="shibata" width="300" height="auto"/>
+                    <img src="https://students.cs.niu.edu/~z1977897/shibata.jpg" alt="shibata" width="300" height="auto"/>
                     <form method="post">
                         <button class="button button1" type="submit" name="addtocart">ADD TO CART</button>
                         <input type="hidden" name="stuffie_id" value="S008">
@@ -273,7 +273,7 @@ if(isset($_POST['addtocart']))
 
                 <td>
                     <br>
-                    <img src="https://media.discordapp.net/attachments/1488268700904849598/1495956492434800670/IMG_8170.jpg?ex=69e8218d&is=69e6d00d&hm=094adb1acdee0f7e188e2846ce31ecd4e70590549d740462a52369a2caac216d&=&format=webp&width=1152&height=864" alt="shibata BIG" width="300" height="auto"/>
+                    <img src="https://students.cs.niu.edu/~z1977897/shibata%20big.jpg" alt="shibata BIG" width="300" height="auto"/>
                     <form method="post">
                         <button class="button button1" type="submit" name="addtocart">ADD TO CART</button>
                         <input type="hidden" name="stuffie_id" value="S009">
@@ -289,7 +289,7 @@ if(isset($_POST['addtocart']))
             <tr>
                 <td>
                     <br>
-                    <img src="https://media.discordapp.net/attachments/1488268700904849598/1495956523464134707/IMG_8177.jpg?ex=69e82194&is=69e6d014&hm=ee9eddcb4b96a8f371328f929190e3f20bd0f43625ad96b3dbec4ac6718e6b92&=&format=webp&width=1152&height=864" alt="burnt chibatta" width="300" height="auto"/>
+                    <img src="https://students.cs.niu.edu/~z1977897/burnt%20shibata.jpg" alt="burnt chibatta" width="300" height="auto"/>
                     <form method="post">
                         <button class="button button1" type="submit" name="addtocart">ADD TO CART</button>
                         <input type="hidden" name="stuffie_id" value="S010">
@@ -303,7 +303,7 @@ if(isset($_POST['addtocart']))
 
                 <td>
                     <br>
-                    <img src="https://media.discordapp.net/attachments/1488268700904849598/1495956524168773752/IMG_8178.jpg?ex=69e82194&is=69e6d014&hm=f930def8bfb399b6bf32308911be8975afea97282a660b42f38c675e285b2d66&=&format=webp&width=1152&height=864" alt="shibata roll" width="300" height="auto"/>
+                    <img src="https://students.cs.niu.edu/~z1977897/shibata%20roll.jpg" alt="shibata roll" width="300" height="auto"/>
                     <form method="post">
                         <button class="button button1" type="submit" name="addtocart">ADD TO CART</button>
                         <input type="hidden" name="stuffie_id" value="S011">
@@ -317,7 +317,7 @@ if(isset($_POST['addtocart']))
 
                 <td>
                     <br>
-                    <img src="https://media.discordapp.net/attachments/1488268700904849598/1495956522713481246/IMG_8176.jpg?ex=69e82194&is=69e6d014&hm=4428f61cd96890883733902fe43f48345cdf158c9b3b7ebb0be371b8741d60c0&=&format=webp&width=1152&height=864" alt="sharkie" width="300" height="auto"/>
+                    <img src="https://students.cs.niu.edu/~z1977897/sharkie.jpg" alt="sharkie" width="300" height="auto"/>
                     <form method="post">
                         <button class="button button1" type="submit" name="addtocart">ADD TO CART</button>
                         <input type="hidden" name="stuffie_id" value="S012">
@@ -333,7 +333,7 @@ if(isset($_POST['addtocart']))
             <tr>
                 <td>
                     <br>
-                    <img src="https://media.discordapp.net/attachments/1488268700904849598/1495956489330757773/IMG_8164.jpg?ex=69e8218c&is=69e6d00c&hm=641fd70476ec228e42e86665c13e2dfae3a0ad2d382efe98c5378a73700e932e&=&format=webp&width=1152&height=864" alt="hedhog" width="300" height="auto"/>
+                    <img src="https://students.cs.niu.edu/~z1977897/hedgehog.jpg" alt="hedhog" width="300" height="auto"/>
                     <form method="post">
                         <button class="button button1" type="submit" name="addtocart">ADD TO CART</button>
                         <input type="hidden" name="stuffie_id" value="S013">
@@ -347,7 +347,7 @@ if(isset($_POST['addtocart']))
 
                 <td>
                     <br>
-                    <img src="https://media.discordapp.net/attachments/1488268700904849598/1495956521723367424/IMG_8175.jpg?ex=69e82194&is=69e6d014&hm=33a8da0292314e44abe2f51b02bb3d9fb2b9207510ed2b5de4428dcbaca9d0cd&=&format=webp&width=1152&height=864" alt="coronavirus" width="300" height="auto"/>
+                    <img src="https://students.cs.niu.edu/~z1977897/corona.jpg" alt="coronavirus" width="300" height="auto"/>
                     <form method="post">
                         <button class="button button1" type="submit" name="addtocart">ADD TO CART</button>
                         <input type="hidden" name="stuffie_id" value="S014">
@@ -361,7 +361,7 @@ if(isset($_POST['addtocart']))
 
                 <td>
                     <br>
-                    <img src="https://media.discordapp.net/attachments/1488268700904849598/1495956520683307088/IMG_8173.jpg?ex=69e82194&is=69e6d014&hm=2c93ed54d5a9be5d03f17028880dc511d84653c6dea6525bd2ff2a326a2f0c90&=&format=webp&width=1152&height=864" alt="skunkie" width="300" height="auto"/>
+                    <img src="https://students.cs.niu.edu/~z1977897/skunkie.jpg" alt="skunkie" width="300" height="auto"/>
                     <form method="post">
                         <button class="button button1" type="submit" name="addtocart">ADD TO CART</button>
                         <input type="hidden" name="stuffie_id" value="S015">
