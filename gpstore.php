46e759ca5d4ab1e26cd5bf391c6ea70066fd189e
gamerlegume <mumu.unicorn@gmail.com>
2026-05-01 21:11:24 -0500

    Update quantity handling in shopping cart

--- gpstore.php
+++ gpstore.php
@@ -16,6 +16,19 @@
     session_start();
     $trackingID = session_id();
 
+    $stmt = $pdo->prepare("SELECT * FROM STUFFEDANIMALSTORE");
+    $stmt->execute();
+    $products = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    if(isset($_POST['qty']))
+    {
+        $qty = $_POST['qty'];
+    }
+    else
+    {
+        $qty = 1;
+    }
+
     if(isset($_POST['addtocart']))
     {
         $stuffieID=$_POST['stuffie_id'];
@@ -37,7 +50,7 @@
         {
             // Calculate new quantity if one more item is added
             $currentQty = (int)$row['CartQty'];
-            $newQty = $currentQty + 1;
+            $newQty = $currentQty + $qty;
 
             // Prevent adding more items than what is available in stock
             if ($newQty > $stockQty)
@@ -61,8 +74,8 @@
             else
             {
                 // Insert new item into cart with quantity of 1
-                $stmt = $pdo->prepare("INSERT INTO SHOPPINGCART (TrackingID, StuffieID, CartQty) VALUES (?, ?, 1)");
-                $stmt->execute([$trackingID, $stuffieID]);
+                $stmt = $pdo->prepare("INSERT INTO SHOPPINGCART (TrackingID, StuffieID, CartQty) VALUES (?, ?, ?)");
+                $stmt->execute([$trackingID, $stuffieID, $qty]);
             }
         }
     }
@@ -274,223 +287,38 @@
 
         <table width="75%" border="0">    
             <tr>
-                <td>
-                    <br>
-                    <img src="https://students.cs.niu.edu/~z1977897/erawr.jpg" alt="erawr" width="300" height="auto"/>
-                    <form method="post">
-                        <button class="button button1" type="submit" name="addtocart">ADD TO CART</button>
-                        <input type="hidden" name="stuffie_id" value="S001">
-                    </form>
+                <?php
+                    $count = 0;
 
-                    <p>erawr</p>
-                    <p>$500.00</p>
-                    <p>green dinosaur</p>
-                    <br>
-                </td>
+                    foreach ($products as $product)
+                    {
+                        //this makes the table 3 wide
+                        if ($count % 3 == 0 && $count != 0)
+                        {
+                            echo "</tr><tr>";
+                        }
 
-                <td>
-                    <br>
-                    <img src="https://students.cs.niu.edu/~z1977897/urawr.jpg" alt="urawr" width="300" height="auto"/>
-                    <form method="post">
-                        <button class="button button1" type="submit" name="addtocart">ADD TO CART</button>
-                        <input type="hidden" name="stuffie_id" value="S002">
-                    </form>
+                        echo "
+                        <td>
+                            <br>
+                            <a href='product.php?id={$product['StuffieID']}'>
+                                <img src='{$product['ImagePath']}' width='300' height='auto'/>
+                            </a>
 
-                    <p>urawr</p>
-                    <p>$67.00</p>
-                    <p>light pink dinosaur</p>
-                    <br>
-                </td>
+                            <form method='post'>
+                                <button class='button button1' type='submit' name='addtocart'>ADD TO CART</button>
+                                <input type='hidden' name='stuffie_id' value='{$product['StuffieID']}'>
+                            </form>
 
-                <td>
-                    <br>
-                    <img src="https://students.cs.niu.edu/~z1977897/mirawr.jpg" alt="mirawr" width="300" height="auto"/>
-                    <form method="post">
-                        <button class="button button1" type="submit" name="addtocart">ADD TO CART</button>
-                        <input type="hidden" name="stuffie_id" value="S003">
-                    </form>
+                            <p>{$product['ProductName']}</p>
+                            <p>\${$product['Price']}</p>
+                            <br>
+                        </td>
+                        ";
 
-                    <p>mirawr</p>
-                    <p>$3.25</p>
-                    <p>pink dinosaur</p>
-                    <br>
-                </td>
-            </tr>
-
-            <tr>
-                <td>
-                    <br>
-                    <img src="https://students.cs.niu.edu/~z1977897/tirawr.jpg" alt="tirawr" width="300" height="auto"/>
-                    <form method="post">
-                        <button class="button button1" type="submit" name="addtocart">ADD TO CART</button>
-                        <input type="hidden" name="stuffie_id" value="S004">
-                    </form>
-
-                    <p>tirawr</p>
-                    <p>$487.75</p>
-                    <p>yellow dinosaur</p>
-                    <br>
-                </td>
-
-                <td>
-                    <br>
-                    <img src="https://students.cs.niu.edu/~z1977897/quagsire.jpg" alt="quagsire" width="300" height="auto"/>
-                    <form method="post">
-                        <button class="button button1" type="submit" name="addtocart">ADD TO CART</button>
-                        <input type="hidden" name="stuffie_id" value="S005">
-                    </form>
-
-                    <p>quagsire</p>
-                    <p>$620.00</p>
-                    <p>quagsire pokemon</p>
-                    <br>
-                </td>
-
-                <td>
-                    <br>
-                    <img src="https://students.cs.niu.edu/~z1977897/clodsire.jpg" alt="clodsire" width="300" height="auto"/>
-                    <form method="post">
-                        <button class="button button1" type="submit" name="addtocart">ADD TO CART</button>
-                        <input type="hidden" name="stuffie_id" value="S006">
-                    </form>
-
-                    <p>clodsire</p>
-                    <p>$980.00</p>
-                    <p>clodsire pokemon</p>
-                    <br>
-                </td>
-            </tr>
-
-            <tr>
-                <td>
-                    <br>
-                    <img src="https://students.cs.niu.edu/~z1977897/tamago.jpg" alt="tamago" width="300" height="auto"/>
-                    <form method="post">
-                        <button class="button button1" type="submit" name="addtocart">ADD TO CART</button>
-                        <input type="hidden" name="stuffie_id" value="S007">
-                    </form>
-
-                    <p>tamago</p>
-                    <p>$495.00</p>
-                    <p>lil egg</p>
-                    <br>
-                </td>
-
-                <td>
-                    <br>
-                    <img src="https://students.cs.niu.edu/~z1977897/shibata.jpg" alt="shibata" width="300" height="auto"/>
-                    <form method="post">
-                        <button class="button button1" type="submit" name="addtocart">ADD TO CART</button>
-                        <input type="hidden" name="stuffie_id" value="S008">
-                    </form>
-
-                    <p>shibata</p>
-                    <p>$420.00</p>
-                    <p>shiba inu</p>
-                    <br>
-                </td>
-
-                <td>
-                    <br>
-                    <img src="https://students.cs.niu.edu/~z1977897/shibata%20big.jpg" alt="shibata BIG" width="300" height="auto"/>
-                    <form method="post">
-                        <button class="button button1" type="submit" name="addtocart">ADD TO CART</button>
-                        <input type="hidden" name="stuffie_id" value="S009">
-                    </form>
-
-                    <p>shibata BIG</p>
-                    <p>$515.00</p>
-                    <p>BIG shiba inu</p>
-                    <br>
-                </td>
-            </tr>
-
-            <tr>
-                <td>
-                    <br>
-                    <img src="https://students.cs.niu.edu/~z1977897/burnt%20shibata.jpg" alt="burnt chibatta" width="300" height="auto"/>
-                    <form method="post">
-                        <button class="button button1" type="submit" name="addtocart">ADD TO CART</button>
-                        <input type="hidden" name="stuffie_id" value="S010">
-                    </form>
-
-                    <p>burnt chibatta</p>
-                    <p>$499.00</p>
-                    <p>black shiba inu</p>
-                    <br>
-                </td>
-
-                <td>
-                    <br>
-                    <img src="https://students.cs.niu.edu/~z1977897/shibata%20roll.jpg" alt="shibata roll" width="300" height="auto"/>
-                    <form method="post">
-                        <button class="button button1" type="submit" name="addtocart">ADD TO CART</button>
-                        <input type="hidden" name="stuffie_id" value="S011">
-                    </form>
-
-                    <p>shibata roll</p>
-                    <p>$455.00</p>
-                    <p>round shiba inu</p>
-                    <br>
-                </td>
-
-                <td>
-                    <br>
-                    <img src="https://students.cs.niu.edu/~z1977897/sharkie.jpg" alt="sharkie" width="300" height="auto"/>
-                    <form method="post">
-                        <button class="button button1" type="submit" name="addtocart">ADD TO CART</button>
-                        <input type="hidden" name="stuffie_id" value="S012">
-                    </form>
-
-                    <p>sharkie</p>
-                    <p>$510.00</p>
-                    <p>shark with a pineapple surfboard</p>
-                    <br>
-                </td>
-            </tr>
-
-            <tr>
-                <td>
-                    <br>
-                    <img src="https://students.cs.niu.edu/~z1977897/hedgehog.jpg" alt="hedhog" width="300" height="auto"/>
-                    <form method="post">
-                        <button class="button button1" type="submit" name="addtocart">ADD TO CART</button>
-                        <input type="hidden" name="stuffie_id" value="S013">
-                    </form>
-
-                    <p>hedhog</p>
-                    <p>$520.00</p>
-                    <p>squeaks, honks, AND crinkles?! perfect for dogs or people with whimsy</p>
-                    <br>
-                </td>
-
-                <td>
-                    <br>
-                    <img src="https://students.cs.niu.edu/~z1977897/corona.jpg" alt="coronavirus" width="300" height="auto"/>
-                    <form method="post">
-                        <button class="button button1" type="submit" name="addtocart">ADD TO CART</button>
-                        <input type="hidden" name="stuffie_id" value="S014">
-                    </form>
-
-                    <p>coronavirus</p>
-                    <p>$2019.00</p>
-                    <p>AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAHHHHHHHHHHHHHH</p>
-                    <br>
-                </td>
-
-                <td>
-                    <br>
-                    <img src="https://students.cs.niu.edu/~z1977897/skunkie.jpg" alt="skunkie" width="300" height="auto"/>
-                    <form method="post">
-                        <button class="button button1" type="submit" name="addtocart">ADD TO CART</button>
-                        <input type="hidden" name="stuffie_id" value="S015">
-                    </form>
-
-                    <p>skunkie</p>
-                    <p>$510.50</p>
-                    <p>BIG FAT GUY he wants love, i know hes expensive but pls buy him</p>
-                    <br>
-                </td>
+                        $count++;
+                    }
+                ?>
             </tr>
         </table>
     </body>
